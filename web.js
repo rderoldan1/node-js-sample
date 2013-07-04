1097ca24994575e9e938fd9a8aff4dc821fbcc4a
Ruben Espinosa <rderoldan1@gmail.com>
2013-07-04 03:58:18 +0000

    index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var file = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(file.toString);
+  response.send(file.toString());
 });
 
 var port = process.env.PORT || 5000;
